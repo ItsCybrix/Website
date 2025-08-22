d81ae61825657f9579c55355b7761810c1f235b7
Cybrix <cybrixtheproot@proton.me>
2025-08-22 01:36:51 -0400

    new version

--- server.js
+++ server.js
@@ -1,131 +1,22 @@
 const express = require('express');
-const config = require('dotenv').config();
-const fs = require('fs')
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('./public/assets'))
-
-var bodyParser = require('body-parser')
-
-var cookies = require('cookie-parser');
-
-const db = require('./lib/dbConnector')
-
-const md5 = require('md5')
-
-const { v4: uuidv4 } = require('uuid');
-
-const siteVersion = fs.readFileSync('./.version')
-
-
-
-
-app.use(cookies());
-
-app.use(require('./middleware/userManager'))
-
-app.use(bodyParser.urlencoded({ extended: false }))
-
-const fileVars = function(req, res, next){
-  res.locals.siteVersion = siteVersion;
-  next();
-}
-
-
-app.use(fileVars);
-
-app.use(function(req, res, next){
-  if(req.cookies.ContentWarnDismissed){
-    res.locals.hideContentWarning = true
-    next();
-  }else{
-    res.locals.hideContentWarning = false
-    next();
-
-  }
-})
-
-// parse application/x-www-form-urlencoded
-
-// parse application/json
-app.use(bodyParser.json())
-
-app.use(require('./middleware/userManager'))
-
-
-
-
-
 app.set('views', './public/views');
 
 app.get('/', (req, res)=>{
-    res.render('index');
-    //console.log(req.cookies);
+    res.render('index')
 })
 
-app.get('/users/login', (req, res)=>{
-
-    res.render('./login');
-
-});
-
-app.post('/users/login', (req, res)=>{
-  console.log(req.body)
-
-  let username = req.body.username
-
-  uuid = uuidv4();
 
 
-  db.query("SELECT * FROM users WHERE username =?", [username.toLowerCase()], function (err, result){
-    if(err){
-      res.send("DB QUERY ERROR: " + err);
-    }
 
-
-    let password = md5(req.body.password)
-
-    if(result.length > 0){
-      if(password == result[0].password){
-        res.cookie('sessionToken', uuid, {maxAge: 1000 * 60 * 86400 * 30 * 12, httpOnly: true})
-
-        db.query("UPDATE users SET sessionToken=? WHERE username=?", [uuid ,req.body.username], function (err, result){
-          if(err) throw err;
-        })
-
-        res.redirect('/');
-      }else{
-        res.redirect('/users/login')
-      }      
-    }
-
-  })
-
-
+app.use('/',(req, res)=>{
+    res.send('404');
 })
 
-app.use('/blog', require('./routes/blog'))
-
-app.use('/admin', require('./routes/admin'))
-
-app.get('/:route', (req, res)=>{
-
-
-
-        db.query("SELECT * FROM pages WHERE url = ?",  [req.params.route], function (err, result) {
-          if (err) {
-            res.send("DB.query.error" + err)
-          }
-
-          if(result.length > 0){
-            res.render('./page-template', {pageBuilderData: result[0]})
-          }else{
-            res.render("./errors/404")
-          }
-        });
-      });
 
 app.listen(5050, ()=>{
-  console.log('Cybrix web service version '+ siteVersion +' is running on local port 5050')
-});
+    console.log("Cybrix.Dev web worker started")
+})
