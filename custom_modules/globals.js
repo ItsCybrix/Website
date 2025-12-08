2537457177d47b2a23b7ac76de3f3d31d180de3e
Cybrix <cybrixtheproot@proton.me>
2025-12-07 22:57:19 -0500

    Blips!

--- custom_modules/globals.js
+++ custom_modules/globals.js
@@ -1,11 +1,26 @@
 const generateMCDCode = require('./Crypto')
-var MCDCode = require('./Crypto')
+const db = require('./sql/db_connector');
 
 function Globals(req, res, next) {
 
 res.locals.MCDCode = generateMCDCode();
 
-next();
+
+    db.query("SELECT * FROM blips", (err, result) => {
+        if (err) {
+            console.error("DB error in middleware:", err);
+            return res.status(500).send("Server error");
+        }
+
+        if (result.length === 0) {
+            console.log("NO BLIPS!")
+        }
+
+        // ✅ Now available in templates
+        res.locals.blips = result;
+        next();
+    });
+
 
 }
 
